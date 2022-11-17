ac8c267cbae1a520f7d6f980b85e3248f7b4eef4
Vinicius Scherer <42481748+MacScherer@users.noreply.github.com>
2022-11-17 01:54:52 -0300

    Create HouseExampleClass.java

---
+++ HouseExampleClass.java
@@ -0,0 +1,12 @@
+package com.example.testenviroment;
+
+class Home {
+    // Properties
+    String color;
+
+    // Methods
+    void openDoor(){
+        System.out.println("Door Opened");
+    }
+
+}
